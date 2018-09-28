8ae73c4c8bcfc72ce1d7b3ba3c205abe1313b864
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-28 22:19:27 +0200

    SillySimon

--- poti.ino
+++ poti.ino
@@ -1,11 +1,11 @@
-int sensorstate = 0
+int sensorstate = 0;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600)
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-snesorstate = analogRead(1)
-Serial.println(sensorstate)
+snesorstate = analogRead(1);
+Serial.println(sensorstate);
 }
