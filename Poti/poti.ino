bd2e1c1f819f5337bd5be6d126dac25ca7da317f
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-29 11:58:53 +0200

    SillySimon

--- Poti/poti.ino
+++ Poti/poti.ino
@@ -8,13 +8,17 @@ Serial.begin(19200);
 void loop() {
   // put your main code here, to run repeatedly:
 sensorstate = analogRead(1);
-if (sensorstate < 500) {
+if (sensorstate <= 500) {
   Serial.println("höher");
   Serial.println(sensorstate);
 }
-if (sensorstate > 500){
+if (sensorstate >= 550){
   Serial.println("tiefer");
   Serial.println(sensorstate);
 }
+/*else{
+  Serial.println("perfekt");
+  Serial.println(sensorstate);
+}*/
 delay(200);
 }
