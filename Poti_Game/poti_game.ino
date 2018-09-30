69c9098943bcb8948f9f53600911bd10d9f274d4
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-30 17:38:47 +0200

    SillySimon

--- Poti_Game/poti_game.ino
+++ Poti_Game/poti_game.ino
@@ -1,12 +1,17 @@
 int sensorstate = 0;
-int policiy = random()
+int policy = random(0,1000);
+int minimum = policy;
+int maximum = policy + 50;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(19200)
+Serial.begin(19200);
+pinMode(13, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 sensorstate = analogRead(1);
-if (sensorstate )
+if (sensorstate < minimum){
+  Serial.println
+}
 }
