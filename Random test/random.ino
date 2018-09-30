9759a9e7ae4840e6c8c94d58731b7ad5f93473aa
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-30 17:16:55 +0200

    SillySimon

--- Random test/random.ino
+++ Random test/random.ino
@@ -1,4 +1,5 @@
 int randval = random(0,500);
+int randval2 = randval + 50;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(19200);
@@ -6,5 +7,11 @@ Serial.begin(19200);
 
 void loop() {
   // put your main code here, to run repeatedly:
+randval = random(0,500);
+randval2 = randval + 50; 
 Serial.println(randval);
+Serial.println("Platzhalter");
+Serial.println(randval2);
+Serial.println("Platzhalter2");
+delay(1000);
 }
