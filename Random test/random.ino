5a54abef882b319f39714208afcf177dad4f79e9
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-30 17:09:21 +0200

    SillySimon

--- Random test/random.ino
+++ Random test/random.ino
@@ -1,10 +1,10 @@
-int random = random(0,500)
+int randval = random(0,500);
 void setup() {
   // put your setup code here, to run once:
-Serial.begin()
+Serial.begin(19200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Serial.println(randval);
 }
