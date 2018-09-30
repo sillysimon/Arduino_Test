9165072da9aabd1611f63abf529aa05d065f2c1e
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-09-30 18:54:43 +0200

    SillySimon

--- Poti_Game/poti_game.ino
+++ Poti_Game/poti_game.ino
@@ -1,4 +1,5 @@
 int sensorstate = 0;
+int buttonstate = 0;
 int policy = random(0,1000);
 int minimum = policy;
 int maximum = policy + 50;
@@ -11,7 +12,22 @@ pinMode(13, INPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 sensorstate = analogRead(1);
+buttonstate = digitalRead(13)
 if (sensorstate < minimum){
-  Serial.println
+  Serial.println("höher");
 }
+else if (sensorstate > maximum){
+  Serial.println("tiefer");
+  Serial.println(sensorstate);
+}
+else (){
+  Serial.println("perfekt");
+}
+if (buttonstate == HIGH){
+  int policy = random(0,1000);
+  int minimum = policy;
+  int maximum = policy + 50;
+  Serial.println("Neuer Wert")
+}
+
 }
