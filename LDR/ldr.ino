acfade91ce2009fc92fe945b8c8fa60f2e980b3c
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-10-06 23:01:57 +0200

    SillySImon

--- LDR/ldr.ino
+++ LDR/ldr.ino
@@ -1,14 +1,20 @@
-int sensorstate = 0
+int sensorstate = 0;
 void setup() {
   // put your setup code here, to run once:
 pinMode(2, OUTPUT);
-Serial.begin(9200);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly
   sensorstate = analogRead(0);
-  Serial.println(sensorstate)
+  Serial.println(sensorstate);
+  if (sensorstate > 400){
+      digitalWrite(2, HIGH);
+  }
+  else {
+      digitalWrite(2, LOW);
+  }
   delay(500);
 
 }
