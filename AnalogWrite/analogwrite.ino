7f71e11d3ef1b549cda23af5deea29bccc0c79f0
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-10-14 19:52:42 +0200

    SillySimon

--- AnalogWrite/analogwrite.ino
+++ AnalogWrite/analogwrite.ino
@@ -2,10 +2,12 @@ int sensorstate
 void setup() {
   // put your setup code here, to run once:
 pinMode(3, OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 sensorstate = analogRead(0);
-analogWrite(3, 10);
+analogWrite(3, sensorstate);
+Serial.println(sensorstate)
 }
