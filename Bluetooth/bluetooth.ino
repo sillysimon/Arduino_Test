2230d75542a95388b18bffaf74499fa500adcf2b
SillySimon <43683094+sillysimon@users.noreply.github.com>
2018-11-13 17:53:37 +0100

    SillySimon

--- Bluetooth/bluetooth.ino
+++ Bluetooth/bluetooth.ino
@@ -1,9 +1,17 @@
 int serialbt;
+int value;
 void setup(){
     Serial.begin(9600);
+    Serial3.begin(9600);
+    pinMode(13, OUTPUT);
 }
 void loop(){
-    serialbt=Serial.read();
-    Serial.println();
-    delay(200);
+    serialbt=Serial3.read();
+    if(serialbt == -1){
+      }
+      else{
+    value= serialbt -49;        
+    Serial.println(value);
+    delay(20);
+      }
 }
