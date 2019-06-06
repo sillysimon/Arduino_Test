da2bb4b0ab47d5b6110e00d3952c659fe69938d0
Simon Waidele <simon@pop-os.localdomain>
2019-06-06 10:27:54 +0200

    Added Pinout

--- Independent Projects/Gamepad/Gamepad.ino
+++ Independent Projects/Gamepad/Gamepad.ino
@@ -1,7 +1,26 @@
+#define Dup 2
+#define Ddown 5
+#define Dleft 3
+#define Dright 4
+#define X 14
+#define Y 15
+#define B 16
+#define A 10
+#define Start 9
+#define Select 8
+#define RT 7
+#define LT 6
+#define LB 1
+#define RB 0
+#define U1x A0
+#define U1y A1
+#define U2x A3
+#define U2y A2
 void setup()
 {
-Serial.begin(9600);
+/* Serial.begin(9600);
+Serial.println("Hallo Welt");*/
 }
 void loop() {
-Serial.println("Hallo Welt");
+
 }
